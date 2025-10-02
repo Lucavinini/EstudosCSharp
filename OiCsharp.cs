a95b130e897299b99cb01372ee1be40257eb13f3
Lucavinini <lucas.00000850855@unicap.br>
2025-10-02 17:39:16 -0300

    feat: Adiciona arquivos de projeto e configuração inicial do C#

---
+++ OiCsharp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Meuespaco
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Hello World! Estou aprendendo C#");
+        }
+    }
+}
